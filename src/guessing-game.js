ccc3f52359d60070d3cf0c2967ef0a85f67d03e9
sasha-kazakevich <ssasshaka@gmail.com>
2017-01-27 23:41:59 +0300

    guessing-game: implement GuessingGame class

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,30 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+      this.min = null;
+      this.max = null;
+    }
 
     setRange(min, max) {
+      this.min = min;
+      this.max = max;
 
+      return this;
     }
 
     guess() {
-
+      return Math.round((this.min + this.max) / 2);
     }
 
     lower() {
+      this.max = this.guess();
 
+      return this;
     }
 
     greater() {
+      this.min = this.guess();
 
+      return this;
     }
 }
 
